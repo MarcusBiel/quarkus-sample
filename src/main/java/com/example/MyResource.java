bbd5b5b2d9633a9106c3ebb93b60244cced4b7e9
Marcus Biel <info@marcus-biel.com>
2020-06-23 19:40:32 +0200

    Fix ClassNotFoundException in native mode

--- src/main/java/com/example/MyResource.java
+++ src/main/java/com/example/MyResource.java
@@ -14,7 +14,30 @@ public class MyResource {
 
     @POST
     @Path("/hulu")
-    public void hulu() {
-        SendGrid sendGrid = new SendGrid("abcd");
+    public Response sendMail() {
+        Email fromEmail = new Email("t888est@kd999kdk.com");
+        Content textContent = new Content(TEXT_HTML, "test");
+        Mail mail = new Mail();
+        mail.setSubject("test");
+        mail.addContent(textContent);
+        mail.setFrom(fromEmail);
+
+        List<String> recipients = List.of("t888est@kd999kdk.com");
+        for (String recipientEmail : recipients) {
+            Personalization personalization = new Personalization();
+            personalization.addTo(new Email(recipientEmail));
+            mail.addPersonalization(personalization);
+        }
+
+        SendGrid sendGrid = new SendGrid("PUT_HERE_SOME_SEND_GRID_API_KEY_FOR_TESTING");
+        Request request = new Request();
+        try {
+            request.setMethod(Method.POST);
+            request.setEndpoint("mail/send");
+            request.setBody(mail.build()); // Body will be empty in native mode
+            return sendGrid.api(request);
+        } catch (IOException ex) {
+            throw new RuntimeException(ex);
+        }
     }
 }
